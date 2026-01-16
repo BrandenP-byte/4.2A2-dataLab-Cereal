8f9cf0715d82275b20dded52a1e15e8495808ffe
BrandenP-byte <pham4878@mydusd.org>
2026-01-16 11:05:52 -0800

    Add standardDeviation function to calculate std dev
    Implement standard deviation calculation for an array.

---
+++ standardDeviation.java
@@ -0,0 +1,20 @@
+public static double standardDeviation (double[] list);
+{
+    // Finding mean of list
+    for (double i; list) {
+        sum += i;
+    }
+    int length = list.length;
+    double mean = sum/length;
+
+    // calculate standard deviation 0 = 
+    double standardDeviation = 0.0;
+    for (double n: list) 
+    {
+        //()
+        standardDeviation += (n - mean, 2);
+        math.sqrt(standardDeviation/length);
+
+    }
+    return standardDeviation;
+}
